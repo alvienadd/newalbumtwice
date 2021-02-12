cc988c7fe3498677ca0038078682d75166b59d88
Alvien Laksono <alvien.add@gmail.com>
2021-02-12 13:05:53 +0700

    Update JSON List 2 New Album Twice

--- lib/Screens/components/DetailPria.dart
+++ lib/Screens/components/DetailPria.dart
@@ -15,26 +15,31 @@ class DetailPria extends StatefulWidget {
 
 class _DetailPriaState extends State<DetailPria> {
 
-   List<DataTokoh> _list;
+  //  List<DataTokoh> _list;
 
   @override
   Widget build(BuildContext context) {
 
     // double width=MediaQuery.of(context).size.width;
     // double height=MediaQuery.of(context).size.height;
-    var id=widget.args.tracks;
+    var tracks=widget.args.tracks;
 
     return Scaffold(
   
         body: ListView.builder(
                   physics: BouncingScrollPhysics(),
-                  itemCount: _list.length,
+                  itemCount: tracks.length,
                   itemBuilder: (context, index) {
                     return Container(
                         child:Column(
-                          
-                        )
-                    )})
+                          children:<Widget>[Text(
+                                  tracks[index].name,
+                                  style: TextStyle(
+                                      color: Colors.pink,
+                                      fontSize: 16),
+                                ),
+                          ])
+                        );})
         
         
         // Center(child: Text("${widget.args.tracks[id].name}"))
